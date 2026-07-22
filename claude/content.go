ec9c774c5e18f2d51774703fa523150642072f6b
Kevin Horst <browniehorst@web.de>
2026-07-22 21:39:31 +0200

    claude: ContentBlock: Added tool_use and tool_result fields

--- claude/content.go
+++ claude/content.go
@@ -1,10 +1,19 @@
 package claude
 
-import "errors"
+import (
+	"encoding/json"
+	"errors"
+)
 
 type ContentBlock struct {
-	Type string `json:"type"`
-	Text string `json:"text"`
+	Id        string          `json:"id"`
+	Content   json.RawMessage `json:"content"`
+	Input     json.RawMessage `json:"input"`
+	IsError   bool            `json:"is_error"`
+	Name      string          `json:"name"`
+	Text      string          `json:"text"`
+	ToolUseId string          `json:"tool_use_id"`
+	Type      string          `json:"type"`
 }
 
 func (b *ContentBlock) Validate() error {
